a9da36ef4517823d89ca1b3c3d33eed45e94604a
Augustine-Joe <85614669+Augustine-Joe@users.noreply.github.com>
2021-06-19 12:59:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var nums = [1,2,3,4,5];
+
+function array_square(a)
+{
+  for(i=0;i<a.length;i++)
+  {
+    a[i]=a[i]*a[i]
+  }
+  return a;
+}
+
+function setup() {
+  createCanvas(400, 400);
+ var result = array_square (nums)
+ console.log(result)
+}
+
+function draw() {
+  background(220);
+}
